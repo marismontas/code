ce3bfd6b54063a4ab6c66278079d49d614f0afd3
marismontas <123301276+marismontas@users.noreply.github.com>
2023-11-08 16:13:33 +0000

    Update script.js

--- login_signup/script.js
+++ login_signup/script.js
@@ -9,3 +9,19 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+var state = false;
+function toggle(passwordId) {
+    var passwordInput = document.getElementById(passwordId);
+    var eyeIcon = document.getElementById("eye");
+
+    if (state) {
+        passwordInput.setAttribute("type", "password");
+        eyeIcon.style.color = "#7a797e";
+        state = false;
+    } else {
+        passwordInput.setAttribute("type", "text");
+        eyeIcon.style.color = "#5887ef";
+        state = true;
+    }
+}
